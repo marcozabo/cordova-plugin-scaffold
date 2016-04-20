81b99aa8edbe9ecd8d3570910c2b64f26b63bd11
mdamelio <marco.damelio@objectway.it>
2016-04-20 16:02:55 +0200

    better callback handling

--- src/ios/Scaffold.m
+++ src/ios/Scaffold.m
@@ -2,27 +2,31 @@
 
 @implementation Scaffold
 
-- (void)customaction:(CDVInvokedUrlCommand*)command
-{
-
+- (void)customaction:(CDVInvokedUrlCommand*)command{
+    
+    NSString* myarg = [[command arguments] objectAtIndex:0];//Example of argument
+   
+    
+    CDVPluginResult* pluginResult = nil;
     NSString* callbackId = [command callbackId];
+    NSString* msg = [NSString stringWithFormat: @"Plugin called!, %@", myarg];
+    
+    
+    
+    if (myarg != nil) {
+        
+        //YOUR CODE HERE
+        NSLog(msg);
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK];
+        
+    } else {
+        
+        pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Arg was null"];
+    }
 
-    //get the parameter "name"
-    NSString* name = [[command arguments] objectAtIndex:0]; 
-
-
-    //
-    // YOUR CODE HERE
-    //
-
-
-
-
-    CDVPluginResult* result = [CDVPluginResult
-                               resultWithStatus:CDVCommandStatus_OK
-                               messageAsString:msg];
-
-    [self success:result callbackId:callbackId];
+    
+    [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
 }
 
 @end
